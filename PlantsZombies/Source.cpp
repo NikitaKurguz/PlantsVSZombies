0bf00d7b4071679a5ba1cf4e15a6ef302228067d
NikitaKurguz <nikitakurguz2016@gmail.com>
2025-12-28 00:57:53 +0300

    подключил SFML

--- PlantsZombies/Source.cpp
+++ PlantsZombies/Source.cpp
@@ -1,1 +1,11 @@
-﻿
+﻿#include <SFML/Graphics.hpp>
+#include <iostream>
+using namespace std;
+using namespace sf;
+int main()
+{
+	RenderWindow window(VideoMode(800, 600), "Plants vs Zombies");
+	sleep(seconds(2));
+	window.close();
+	return 0;
+}
