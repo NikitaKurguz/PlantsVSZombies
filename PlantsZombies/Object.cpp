db8abd4dfe226165b8478331ce4426a2a1d047e4
NikitaKurguz <nikitakurguz2016@gmail.com>
2025-12-28 13:28:00 +0300

    Добавил немного определения класса Object и создал Менедежера

--- PlantsZombies/Object.cpp
+++ PlantsZombies/Object.cpp
@@ -1,1 +1,22 @@
 #include "Object.h"
+
+Object::Object()
+{
+
+}
+
+Object::Object(const Object&)
+{
+}
+
+Object::~Object()
+{
+}
+
+void Object::Update(float t)
+{
+}
+
+void Object::Draw()
+{
+}
--- PlantsZombies/Object.h
+++ PlantsZombies/Object.h
@@ -1,10 +1,20 @@
 #pragma once
-struct Position
-{
-	double x, y;
-};
+#include <SFML/Graphics.hpp>
 class Object
 {
+private:
+	int ID;
+	sf::Vector2f position;
+	
+public:
+	Object();
+	Object(const Object&);
+	virtual ~Object();
 
+	int GetID() const { return ID; }
+	sf::Vector2f GetPosition() const { return position; }
+
+	virtual void Update(float t);
+	void Draw();
 };
 
