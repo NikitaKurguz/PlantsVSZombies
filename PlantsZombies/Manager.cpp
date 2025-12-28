a1723b624f5dd2a44279be0a4be2610eaaf0a796
NikitaKurguz <nikitakurguz2016@gmail.com>
2025-12-28 16:55:54 +0300

    Создал менеджера

--- PlantsZombies/Manager.cpp
+++ PlantsZombies/Manager.cpp
@@ -1,1 +1,25 @@
 #include "Manager.h"
+Manager* Manager::instance = nullptr;
+Manager::Manager()
+{
+}
+
+Manager::~Manager()
+{
+	for (auto& obj : objects) delete obj;
+	objects.clear();
+
+	for (auto& m : messages) delete m;
+	messages.clear();
+}
+
+Manager* Manager::GetInstance()
+{
+	if (instance == nullptr) instance = new Manager();
+	return instance;
+}
+
+void Manager::KillInstance()
+{
+	if (instance) delete instance;
+}
--- PlantsZombies/Manager.h
+++ PlantsZombies/Manager.h
@@ -1,8 +1,21 @@
 #pragma once
 #include "Object.h"
+#include "message.h"
+#include <vector>
 class Manager
 {
 private:
+	static Manager* instance;
 
+	std::vector<Object*> objects;
+	std::vector<Message*> messages;
+
+	Manager();
+	~Manager();
+	Manager(const Manager&) = delete;
+	Manager& operator=(const Manager&) = delete;
+public:
+	static Manager* GetInstance();
+	void KillInstance();
 };
 
