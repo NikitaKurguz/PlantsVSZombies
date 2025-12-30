e838c74dca2225c2bfc48dfa85e5547de210f847
NikitaKurguz <nikitakurguz2016@gmail.com>
2025-12-30 12:38:39 +0300

    добавил конструктор создания для объекта и текстуры объекта

--- PlantsZombies/Object.cpp
+++ PlantsZombies/Object.cpp
@@ -2,9 +2,11 @@
 
 int Object::LastID = 0;
 
-Object::Object()
+Object::Object(sf::Vector2f position, float size_rad, const sf::Texture& tex):
+	position(position), size_rad(size_rad)
 {
-
+	id = GetNewID();
+	SetTexture(tex);
 }
 
 Object::Object(const Object& other)
@@ -14,6 +16,27 @@ Object::Object(const Object& other)
 
 Object::~Object()
 {
+}
+
+void Object::SetTexture(const sf::Texture& tex)
+{
+	sprite.setTexture(tex);
+	sf::FloatRect bounds = sprite.getLocalBounds();
+	sprite.setOrigin(bounds.width / 2.0f, bounds.height / 2.0f);
+	if (size_rad > 0)
+	{
+		float maxSide = std::max(bounds.width, bounds.height);
+		if (maxSide > 0)
+		{
+			float scale = (size_rad * 2.0f) / maxSide;
+			sprite.setScale(scale, scale);
+		}
+	}
+
+}
+void Object::Position(sf::Vector2f new_pos)
+{
+	position = new_pos;
 }
 
 int Object::GetNewID()
@@ -34,6 +57,8 @@ void Object::SendMessage(Message* m)
 {
 }
 
-void Object::Draw()
+void Object::Draw(sf::RenderWindow& window)
 {
+	sprite.setPosition(position);
+	window.draw(sprite);
 }
--- PlantsZombies/Object.h
+++ PlantsZombies/Object.h
@@ -6,21 +6,25 @@ struct Object
 private:
 	static int LastID;
 protected:
-	int ID;
+	int id;
+	float size_rad;
 	sf::Vector2f position;
+	sf::Sprite sprite;
 	
 public:
-	Object();
+	Object(sf::Vector2f position, float size_rad, const sf::Texture& tex);
 	Object(const Object& other);
 	virtual ~Object();
 
-	int GetID() const { return ID; }
+	int GetID() const { return id; }
 	sf::Vector2f GetPosition() const { return position; }
-
+	void Position(sf::Vector2f new_pos);
+	
+	void SetTexture(const sf::Texture& tex);
 	static int GetNewID();
 	static int GetLastID();
 	virtual void Update(float t) = 0;
 	virtual void SendMessage(Message* m) = 0;
-	void Draw();
+	void Draw(sf::RenderWindow& window);
 };
 
